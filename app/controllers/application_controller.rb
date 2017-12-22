3d1e86eef62b7b8ccf62172d62b032cf6839edcb
Shivraj <itscool.sid@gmail.com>
2017-12-22 12:10:54 +0530

    setup devise properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   prepend_view_path Rails.root.join("frontend")
-
+  before_action :authenticate_user!
 end
