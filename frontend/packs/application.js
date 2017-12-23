e51249da25727f165a8e1b1b73e76dec8e5e09f3
Shivraj <itscool.sid@gmail.com>
2017-12-23 21:46:24 +0530

    angular test code is working

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -9,22 +9,24 @@
 import "./application.css";
 import "init";
 
-var coreJS = require('core-js');
-var zoneJS = require('zone.js');
-var reflectMetadata = require('reflect-metadata');
-var ng  = {
-      core: require("@angular/core"),
-      common: require("@angular/common"),
-      compiler: require("@angular/compiler"),
-      forms: require("@angular/forms"),
-      platformBrowser: require("@angular/platform-browser"),
-      platformBrowserDynamic: require("@angular/platform-browser-dynamic"),
-      router: require("@angular/router")
+var coreJS = require("core-js");
+var zoneJS = require("zone.js");
+var reflectMetadata = require("reflect-metadata");
+var ng = {
+  core: require("@angular/core"),
+  common: require("@angular/common"),
+  compiler: require("@angular/compiler"),
+  forms: require("@angular/forms"),
+  platformBrowser: require("@angular/platform-browser"),
+  platformBrowserDynamic: require("@angular/platform-browser-dynamic"),
+  router: require("@angular/router")
 };
 
-var AngularTestComponent = ng.core.Component({
-  selector: "shine-angular-test",
-    template: '\
+var AngularTestComponent = ng.core
+  .Component({
+    selector: "shine-angular-test",
+    template:
+      '\
     <h2 *ngIf="name">Hello {{name}}!</h2> \
     <form> \
       <div class="form-group"> \
@@ -34,28 +36,29 @@ var AngularTestComponent = ng.core.Component({
      </div> \
    </form> \
   '
-}).Class({
-  constructor: function() {
-  this.name = null;
-  }
-});
+  })
+  .Class({
+    constructor: function() {
+      this.name = null;
+    }
+  });
 
-var AngularTestAppModule = ng.core.NgModule({
-  imports: [ ng.platformBrowser.BrowserModule, ng.forms.FormsModule ],
-  declarations: [ AngularTestComponent ],
-  bootstrap: [ AngularTestComponent ]
-})
-.Class({
-  constructor: function() {}
-});
+var AngularTestAppModule = ng.core
+  .NgModule({
+    imports: [ng.platformBrowser.BrowserModule, ng.forms.FormsModule],
+    declarations: [AngularTestComponent],
+    bootstrap: [AngularTestComponent]
+  })
+  .Class({
+    constructor: function() {}
+  });
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   var shouldBootstrap = document.getElementById("angular-test");
   if (shouldBootstrap) {
-    ng.platformBrowserDynamic.
-      platformBrowserDynamic().
-      bootstrapModule(AngularTestAppModule);
+    ng.platformBrowserDynamic
+      .platformBrowserDynamic()
+      .bootstrapModule(AngularTestAppModule);
   }
 });
 
